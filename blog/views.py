45fa9c66b19202eab2337569d2bbd8aa8d9ad736
BayanAbaoub <bayaaaaan07@gmail.com>
2026-01-15 20:13:57 +0000

    Deployment commit

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePage(TemplateView):
+    """
+    Displays home page"
+    """
+    template_name = 'index.html'
