78c3b6627ac14e05afee895d91bfac7d99ec4e6b
BayanAbaoub <bayaaaaan07@gmail.com>
2026-01-15 20:34:49 +0000

    Update settings.py to enable deployment

--- capstone_project/settings.py
+++ capstone_project/settings.py
@@ -30,10 +30,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    "capstone-project-blog-7fbe8833f003.herokuapp.com"
+    '.herokuapp.com',
+    '127.0.0.1',
 ]
 
 
