ac072e54e3778a2b19c2abcfc97a4362325c6762
BayanAbaoub <bayaaaaan07@gmail.com>
2026-01-19 22:28:53 +0000

    added comment edit/delete functionality

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -37,3 +38,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
+    
+    def profile_page(request):
+        user = get_object_or_404(User, user=request.user)
+        comments = user.commenter.all()
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
-from .models import Review
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Review, Comment
+from .forms import CommentForm
 
 # Create your views here.
 class ReviewList(generic.ListView):
@@ -24,9 +27,62 @@ def review_detail(request, slug):
 
     queryset = Review.objects.filter(status=1)
     review = get_object_or_404(queryset, slug=slug)
+    comments = review.comments.all().order_by("-created_on")
+    comment_count = review.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.review = review
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
 
     return render(
         request,
         "blog/review_detail.html",
-        {"review": review},
+        {"review": review, "comments": comments, "comment_count": comment_count, "comment_form": comment_form},
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Review.objects.filter(status=1)
+        review = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.review = review
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('review_detail', args=[slug]))
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Review.objects.filter(status=1)
+    review = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('review_detail', args=[slug]))
