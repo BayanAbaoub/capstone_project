0ff8d96a57c59dfc0c68281bfe0dbd097c6fa87f
BayanAbaoub <bayaaaaan07@gmail.com>
2026-01-23 15:22:36 +0000

    test forms

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,13 +1,47 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 from .forms import CommentForm
+from .models import Review
 
 
 class TestCommentForm(TestCase):
 
+    def setUp(self):
+        """Create test user and review"""
+        self.user = User.objects.create_user(
+            username="myUsername",
+            password="myPassword",
+            email="test@test.com"
+        )
+        self.review = Review.objects.create(
+            title="Movie Review Title", 
+            author=self.user,
+            slug="movie-review-title", 
+            excerpt="Movie review excerpt",
+            content="Movie review content", 
+            status=1
+        )
+
     def test_form_is_valid(self):
-        comment_form = CommentForm({'body': 'This is a great post'})
+        comment_form = CommentForm({'body': 'This is a great review'})
         self.assertTrue(comment_form.is_valid())
 
     def test_form_is_invalid(self):
         comment_form = CommentForm({'body': ''})
         self.assertFalse(comment_form.is_valid(), msg="Form is valid")
+    
+    def test_successful_comment_submission(self):
+        """Test for posting a comment on a review"""
+        self.client.login(
+            username="myUsername", password="myPassword")
+        review_data = {
+            'body': 'This is a test comment.'
+        }
+        response = self.client.post(reverse(
+            'review_detail', args=['movie-review-title']), review_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Comment submitted and awaiting approval',
+            response.content
+        )
--- submit/test_forms.py
+++ submit/test_forms.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Submit
 from .forms import SubmitForm
 
 
@@ -12,3 +14,19 @@ class TestSubmitForm(TestCase):
             'submission': 'Hello!'
         })
         self.assertTrue(form.is_valid(), msg="Form is not valid")
+
+    def setUp(self):
+        """Creates submit page content"""
+        self.submit_content = Submit(
+            title="Submit a Review", 
+            content="Share your movie reviews with us!"
+        )
+        self.submit_content.save()
+
+    def test_render_submit_page_with_submit_form(self):
+        """Verifies get request for submit page containing a submit form"""
+        response = self.client.get(reverse('submit'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Submit a Review', response.content)
+        self.assertIsInstance(
+            response.context['submit_form'], SubmitForm)
